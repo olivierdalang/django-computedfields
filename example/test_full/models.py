70b13f1f908fcfc1e51691b4411b08813a21aef4
olivierdalang <olivier.dalang@gmail.com>
2020-04-09 12:41:44 +0200

    adds a failing test when moving children

--- example/test_full/models.py
+++ example/test_full/models.py
@@ -157,3 +157,11 @@ class PartialUpdateB(ComputedFieldsModel):
     @computed(models.CharField(max_length=32), depends=['f_ba#name'])
     def comp(self):
         return self.f_ba.name + self.name
+
+class Parent(ComputedFieldsModel):
+    @computed(models.IntegerField(default=0), depends=['children#parent'])
+    def children_count(self):
+        return self.children.all().count()
+
+class Child(ComputedFieldsModel):
+    parent = models.ForeignKey(Parent, related_name='children', on_delete=models.CASCADE)
